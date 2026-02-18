32d41a3d260e125e7bff892d227cdeb9b2b806cb
kate <kate@noreply.codeberg.org>
2026-02-18 19:17:58 +0100

    changed: markdown size

--- apps/web/src/routes/course/[courseName]/skill/[skillName]/introduction/+page.svelte
+++ apps/web/src/routes/course/[courseName]/skill/[skillName]/introduction/+page.svelte
@@ -38,21 +38,28 @@
 	}
 </script>
 
+
 {#if !loading}
   <div style="
     display: flex;
     flex-direction: column;
-    min-height: 100vh; /* full viewport height */
+    min-height: 100vh;
   ">
     <MarkDownPage
       {readmeHTML}
       {title}
       description={$_('about.meta.description')}
       style="
-        flex: 1; /* fills the left space */
+        flex: 1; /* Füllt den verfügbaren Platz */
+        background-color: white;
+        border-radius: 8px;
+        margin: 1rem;
+        padding: 1rem;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        display: flex;
+        flex-direction: column;
       "
     >
-      <div style="flex: 1;"></div>
       <div style="margin-top: auto; padding: 1rem 0; text-align: center;">
         <Button style="secondary" href={homepageLink}>Go back to course</Button>
         <Button style="primary" href={`/course/${courseName}/skill/${practiceHref}${gistId ? `?gistId=${gistId}` : ''}`}>
@@ -62,3 +69,4 @@
     </MarkDownPage>
   </div>
 {/if}
+
